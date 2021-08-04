60e7c38ea6fac23bb444657f2260ee67a1d46794
Jayant Kumar Naik <jayantnaik98@gmail.com>
2021-08-04 21:37:02 +0530

    update

--- Backend/database/mongoose.js
+++ Backend/database/mongoose.js
@@ -2,7 +2,7 @@
 
 const mongoose = require("mongoose");
 mongoose
-  .connect("mongodb://localhost:27017/TaskManager", {
+  .connect(process.env.MONGODB_URI || "mongodb://localhost:27017/TaskManager", {
     useUnifiedTopology: true,
     useNewUrlParser: true,
   })
