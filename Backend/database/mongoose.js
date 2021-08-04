33e687e0f02341510b5f8da6ff1eefd6acb5fae3
Jayant Kumar Naik <jayantnaik98@gmail.com>
2021-08-04 23:21:00 +0530

    update

--- Backend/database/mongoose.js
+++ Backend/database/mongoose.js
@@ -1,6 +1,7 @@
 // This file will handle connection logic to MongoDB database
 
 const mongoose = require("mongoose");
+const MONGODB_URI = "mongodb+srv://Jayant:jayantknaik@cluster0.f4lrn.mongodb.net/test"
 mongoose
   .connect(process.env.MONGODB_URI || "mongodb://localhost:27017/TaskManager", {
     useUnifiedTopology: true,
